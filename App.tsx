f737df3f52b2ed0d854177e12cbe4cd994c34826
kkamara <kamaracomputers@gmail.com>
2024-01-05 16:57:12 +0000

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -7,14 +7,15 @@ import {
 } from "react-native"
 import FlatCards from "./components/FlatCards"
 import ElevatedCards from "./components/ElevatedCards"
+import FancyCard from "./components/FancyCard"
 
 export default function App() {
     return (
         <SafeAreaView>
             <ScrollView>
-                <Text>App</Text>
                 <FlatCards />
                 <ElevatedCards />
+                <FancyCard />
             </ScrollView>
         </SafeAreaView>
     )

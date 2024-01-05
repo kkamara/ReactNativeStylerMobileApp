48c8d6e8dab7579672bd4b6be0dd35a21a82f3a6
kkamara <kamaracomputers@gmail.com>
2024-01-05 17:26:11 +0000

    Styling cards in React Native

--- components/FancyCard.tsx
+++ components/FancyCard.tsx
@@ -22,16 +22,57 @@ export default function FancyCard() {
 }
 
 const styles = StyleSheet.create({
-    headingText: {},
-    card: {},
-    cardElevated: {},
+    headingText: {
+        fontSize: 24,
+        fontWeight: "bold",
+        paddingHorizontal: 8,
+    },
+    card: {
+        width: 350,
+        height: 360,
+        borderRadius: 6,
+        marginVertical: 12,
+        marginHorizontal: 16,
+    },
+    cardElevated: {
+        backgroundColor: "#fff",
+        elevation: 3,
+        shadowOffset: {
+            width: 1,
+            height: 1,
+        },
+    },
     cardImage: {
         height: 180,
         width: "100%",
+        marginBottom: 8,
+        borderTopLeftRadius: 6,
+        borderTopRightRadius: 6,
     },
-    cardBody: {},
-    cardTitle: {},
-    cardLabel: {},
-    cardDescription: {},
-    cardFooter: {},
+    cardBody: {
+        flex: 1,
+        flexGrow: 1,
+        paddingHorizontal: 12,
+    },
+    cardTitle: {
+        color: "#000",
+        fontSize: 22,
+        fontWeight: "bold",
+        marginBottom: 4,
+    },
+    cardLabel: {
+        color: "#000",
+        fontSize: 14,
+        marginBottom: 6,
+    },
+    cardDescription: {
+        color: "#242B2E",
+        fontSize: 12,
+        marginBottom: 12,
+        marginTop: 6,
+        flexShrink: 1,
+    },
+    cardFooter: {
+        color: "#000",
+    },
 })

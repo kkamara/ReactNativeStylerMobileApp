0a4c4ddbacb4393ad81855c6ddc612618fba0bde
kkamara <kamaracomputers@gmail.com>
2024-01-05 18:04:31 +0000

    Designing Blog Card and buttons

--- components/ActionCard.tsx
+++ components/ActionCard.tsx
@@ -55,15 +55,57 @@ export default function ActionCard() {
 }
 
 const styles = StyleSheet.create({
-    headingText: {},
-    card: {},
-    elevatedCard: {},
-    headingContainer: {},
-    headerText: {},
+    headingText: {
+        fontSize: 24,
+        fontWeight: "bold",
+        paddingHorizontal: 8,
+    },
+    card: {
+        width: 350,
+        height: 360,
+        borderRadius: 6,
+        marginVertical: 12,
+        marginHorizontal: 16,
+    },
+    elevatedCard: {
+        backgroundColor: "#E07C24",
+        elevation: 3,
+        shadowOffset: {
+            width: 1,
+            height: 1,
+        },
+        shadowColor: "#333",
+        shadowOpacity: 0.4,
+    },
+    headingContainer: {
+        height: 40,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    headerText: {
+        color: "#000",
+        fontSize: 16,
+        fontWeight: "600",
+    },
     cardImage: {
         height: 100,
     },
-    bodyContainer: {},
-    footerContainer: {},
-    socialLinks: {},
+    bodyContainer: {
+        padding: 10,
+    },
+    footerContainer: {
+        padding: 8,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+    },
+    socialLinks: {
+        fontSize: 16,
+        color: "#000",
+        backgroundColor: "#fff",
+        paddingHorizontal: 20,
+        paddingVertical: 6,
+        borderRadius: 6,
+    },
 })
